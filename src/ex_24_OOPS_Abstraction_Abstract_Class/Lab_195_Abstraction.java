29f6349b8975c97a3a1fb7acffd08b59b060f66d
Ashwikadk <ashwikadk017@gmail.com>
2025-07-08 07:27:10 +0530

    July 7

---
+++ src/ex_24_OOPS_Abstraction_Abstract_Class/Lab_195_Abstraction.java
@@ -0,0 +1,32 @@
+package ex_24_OOPS_Abstraction_Abstract_Class;
+
+public class Lab_195_Abstraction {
+    public static void main(String[] args) {
+Father f1=new Child();
+Child child=new Child();
+Car c=new Car();
+    }
+}
+abstract class Father{
+
+    abstract void loan50k();//abstract method
+
+    void loan25k(){
+        System.out.println("25k given ");//concretemethod
+                    }
+
+
+}
+class Child extends Father
+{
+    void loan50k(){
+        System.out.println("Son has given 50k");
+    }
+}
+class Car{
+    // This is concreate class
+    // this doesn't have the abstract methods
+    void done(){
+
+    }
+}
